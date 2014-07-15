713856a0e1ade660199cb554b40e024cbff3c3c9
Philipp Meyer <philipp.j.meyer@gmail.com>
2014-07-15 11:10:14 +0200

    Added benchmarks given by Karsten Wolf

---
+++ benchmarks/service-tech/run-benchmarks-lola.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#benchmarks=( 'ibm-soundness' 'sap-reference' )
+#benchmarks=( 'sap-reference' )
+benchmarks=( 'ibm-soundness' )
+extensions=( 'pnet' 'tpn' 'lola' )
+executable='/home/philipp/local/lola-2.0/src/lola '
+
+for benchmark in ${benchmarks[@]}; do
+  benchmark_dir="$benchmark"
+  >$benchmark_dir/positive-lola.list
+  >$benchmark_dir/negative-lola.list
+  >$benchmark_dir/timeout-lola.list
+  >$benchmark_dir/error-lola.list
+  for ext in ${extensions[@]}; do
+    for file in `find $benchmark_dir -name "*.$ext"`; do
+      T="$(date +%s%N)"
+      if [ -e $benchmark_dir/final_marking.lola.safe.task ]; then
+          final=$benchmark_dir/final_marking.lola.safe.task
+      elif [ -e $file.safe.task ]; then
+          final=$file.safe.task
+      fi
+      (
+        set -o pipefail;
+        $executable -f $final $file --markinglimit=1000000 --stubborn=deletion 2>&1 | tee $file.out
+      )
+      result=$?
+      ryes=$(grep "result: yes" $file.out)
+      rno=$(grep "result: no" $file.out)
+      T=$(($(date +%s%N)-T))
+      if [[ -n $ryes ]]; then
+          list='positive'
+      elif [[ -n $rno ]]; then
+          list='negative'
+      elif [[ result -eq 2 ]]; then
+          list='timeout'
+      else
+          list='error'
+      fi
+      echo $T $file >>$benchmark_dir/$list-lola.list
+    done
+  done
+done
---
+++ benchmarks/service-tech/run-benchmarks.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#benchmarks=( 'ibm-soundness' 'sap-reference' )
+#benchmarks=( 'sap-reference' )
+benchmarks=( 'ibm-soundness' )
+extensions=( 'pnet' 'tpn' 'lola' )
+executable='../../slapnet'
+
+for benchmark in ${benchmarks[@]}; do
+  benchmark_dir="$benchmark"
+  >$benchmark_dir/positive-slapnet.list
+  >$benchmark_dir/dontknow-slapnet.list
+  >$benchmark_dir/timeout-slapnet.list
+  >$benchmark_dir/error-slapnet.list
+  for ext in ${extensions[@]}; do
+    for file in `find $benchmark_dir -name "*.$ext"`; do
+      T="$(date +%s%N)"
+      if [ -e $benchmark_dir/final_marking.lola.fin.task ]; then
+          final=$benchmark_dir/final_marking.lola.fin.task
+      elif [ -e $file.fin.task ]; then
+          final=$file.fin.task
+      fi
+      (
+        set -o pipefail;
+        timeout 60 $executable --$ext --safe $file | tee $file.out
+      )
+      result=$?
+      T=$(($(date +%s%N)-T))
+      if [[ result -eq 0 ]]; then
+          list='positive'
+      elif [[ result -eq 2 ]]; then
+          list='dontknow'
+      elif [[ result -eq 124 || result -eq 137 ]]; then
+          list='timeout'
+      else
+          list='error'
+      fi
+      echo $T $file >>$benchmark_dir/$list-slapnet.list
+    done
+  done
+done
