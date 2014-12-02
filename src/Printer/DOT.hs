bcc8770ef5ac215d89feb4e384a4ef02d9306709
Philipp Meyer <philipp.j.meyer@gmail.com>
2014-12-02 15:25:29 +0100

    Added labels and initial marking to DOT printer

--- src/Printer/DOT.hs
+++ src/Printer/DOT.hs
@@ -9,8 +9,8 @@ import Data.ByteString.Builder
 import Data.Monoid
 
 import PetriNet
-import Structure
 
+-- TODO: mark initially labeled places
 renderNet :: PetriNet -> Builder
 renderNet net =
             "digraph petrinet {\n" <>
@@ -18,11 +18,14 @@ renderNet net =
             mconcat (map transLabel (transitions net)) <>
             "}\n"
         where
-            placeLabel p = stringUtf8 p <>  " [label=\" \"];\n"
-            transLabel t = stringUtf8 t <>  " [label=\" \", shape=box, " <>
-                "style=filled, fillcolor=\"" <>
-                (if checkParallelT net t then "#FF0000" else "#AAAAAA") <>
-                "\"];\n" <>
+            placeLabel p = stringUtf8 p <>  " [label=\"" <> stringUtf8 p <>
+                (if initial net p > 0 then
+                    "(" <> integerDec (initial net p) <> ")"
+                 else
+                    ""
+                ) <> "\"];\n"
+            transLabel t = stringUtf8 t <>  " [label=\"" <> stringUtf8 t <> "\", shape=box, " <>
+                "style=filled, fillcolor=\"#AAAAAA\"];\n" <>
                 mconcat (map (\p -> arcLabel (p,t)) (pre net t)) <>
                 mconcat (map (\p -> arcLabel (t,p)) (post net t))
             arcLabel (a,b) = stringUtf8 a <> " -> " <> stringUtf8 b <> "\n"
